ac5aecc60b5ee0a993944b6864b07dbecc4ec3f9
kmeenaks <kmeenaks@gmail.com>
2021-07-11 16:34:42 +0000

    changed the source back

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Welcome to Hello World!";
+    private final String message = "Hello World!";
 
     public App() {}
 
