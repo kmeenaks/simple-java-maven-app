33cba8b3674a05970dbbdeaa2c6fded23ad3579f
kmeenaks <kmeenaks@gmail.com>
2021-07-18 14:07:13 +0000

    Fixed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Kumar, Welcome to the World!";
+    private final String message = "Hello World!";
 
     public App() {}
 
